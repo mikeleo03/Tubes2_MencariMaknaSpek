0732f7154233c8be9597afb3aefd2be67bd62356
GoDillonAudris512 <110383663+GoDillonAudris512@users.noreply.github.com>
2023-03-23 23:51:42 +0700

    refactor again: 1 last folder to clean, also delete unnecesary import line

--- src/Application/App.cs
+++ src/Application/App.cs
@@ -1,18 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Timers;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
-using System.Drawing.Drawing2D;
 using TreasureHunt.Container;
 using TreasureHunt.Algorithm;
-using System.Threading;
 using System.Diagnostics;
 
 namespace TreasureHunt 
--- src/Container/Coordinate.cs
+++ src/Container/Coordinate.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TreasureHunt.Container {
+﻿namespace TreasureHunt.Container {
     // Kelas Coordinate
     class Coordinate {
         // Atribut kelas
