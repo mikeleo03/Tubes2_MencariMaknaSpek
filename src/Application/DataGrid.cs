1f27ed438af83509f3f49319909f22f757c8f234
GoDillonAudris512 <110383663+GoDillonAudris512@users.noreply.github.com>
2023-03-24 00:03:28 +0700

    move the datagrid.cs to its appropriate folder

--- src/Application/DataGrid.cs
+++ src/Application/DataGrid.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-using System.Drawing;
-using System.Windows.Forms;
-using System.Drawing.Drawing2D;
+﻿using System.Data;
 
 namespace TreasureHunt {
     class DataGrid {
